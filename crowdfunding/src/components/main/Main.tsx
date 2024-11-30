6e616e08bead4be463c9ecffccbf664703e87d92
David-Chazoule <d.chazoule@gmail.com>
2024-11-30 17:46:23 +0100

    title-component basic desing

--- crowdfunding/src/components/main/Main.tsx
+++ crowdfunding/src/components/main/Main.tsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import TitleComponent from '../titleComponent/TitleComponent'
-import CounterComponent from '../counterComponent/CounterComponent'
-import CrowdfundingContainer from '../crowdfundingContainer/CrowdfundingContainer'
+import React from "react";
+import TitleComponent from "../titleComponent/TitleComponent";
+import CounterComponent from "../counterComponent/CounterComponent";
+import CrowdfundingContainer from "../crowdfundingContainer/CrowdfundingContainer";
 
 function Main() {
   return (
-    <div>
-    <TitleComponent/>
-    <CounterComponent/>
-    <CrowdfundingContainer/>
-
-
-
-
+    <div className="main">
+      <div className="container">
+        <TitleComponent />
+        <CounterComponent />
+        <CrowdfundingContainer />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
--- crowdfunding/src/components/titleComponent/TitleComponent.tsx
+++ crowdfunding/src/components/titleComponent/TitleComponent.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import logoMaster from "../../styles/images/logo-mastercraft.svg";
+import iconBookmarked from "../../styles/images/icon-bookmark.svg";
 
 function TitleComponent() {
   return (
     <div className="title-box">
+      <img className="logoMaster" src={logoMaster} alt="logo-mastercraft" />
       <h2>Mastercraft Bamboo Monitor Riser</h2>
       <p>
         A beautiful & handcrafted monitor stand to reduce neck and eye strain.
       </p>
       <div className="footer-title-box">
         <button>Back this project</button>{" "}
-        <div className="bookmaked">Bookmark</div>
+        <div className="bookmaked">
+          <img src={iconBookmarked} alt="icon-bookmark" />
+
+          <span>Bookmarked</span>
+        </div>
       </div>
     </div>
   );
